b38edbd299a1b70d4e7bf571fc71b32420783721
Daniel Wysocki <dwysocki@oswego.edu>
2015-05-29 16:27:47 -0400

    Added some formatting settings

--- init.el
+++ init.el
@@ -14,9 +14,8 @@
 ;; custom scratch buffer message
 (setq initial-scratch-message
       (with-temp-buffer
-	(insert-file-contents "~/.emacs.d/logo.txt")
-	(buffer-string)))
-
+        (insert-file-contents "~/.emacs.d/logo.txt")
+        (buffer-string)))
 
 
 ;;
@@ -28,6 +27,10 @@
 
 ;; hide toolbar
 (tool-bar-mode -1)
+
+;; display line and column numbers
+(line-number-mode)
+(column-number-mode)
 
 
 ;;
@@ -81,11 +84,11 @@
   (autoload 'enable-paredit-mode "paredit"
     "Turn on pseudo-structural editing of Lisp code." t)
   (dolist (hook '(emacs-lisp-mode-hook
-		  eval-expression-minibuffer-setup-hook
-		  ielm-mode-hook
-		  lisp-mode-hook
-		  lisp-interaction-mode-hook
-		  scheme-mode-hook))
+                  eval-expression-minibuffer-setup-hook
+                  ielm-mode-hook
+                  lisp-mode-hook
+                  lisp-interaction-mode-hook
+                  scheme-mode-hook))
     (add-hook hook #'enable-paredit-mode)))
 
 (use-package python-mode
@@ -99,3 +102,21 @@
 
 ; makes scrolling past the end of a page jump to the next page
 (setf doc-view-continuous t)
+
+;;
+;; -- formatting --
+;;
+
+;; tabs are evil
+(setq-default indent-tabs-mode nil)
+
+;; set tab width to 4
+(setq-default tab-width 4)
+
+;; show trailing whitespace
+(setq-default show-trailing-whitespace t)
+
+;; whitespace-mode
+(require 'whitespace)
+(setq whitespace-style '(face empty tabs lines-tail trailing))
+(global-whitespace-mode t)
